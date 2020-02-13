c0c4ee6cec281764f729ea4a1e4542a1ee248af5
jiebaomaster <jiebaomaster@163.com>
2020-02-13 18:51:29 +0800

    [add]剑指offer-cp3-反转链表，增加递归的方式

--- jianzhi_offer/cp3/reverseList.c
+++ jianzhi_offer/cp3/reverseList.c
@@ -20,13 +20,36 @@ void reverseList(LinkList *pL) {
     *pL = pPreNode;
 }
 
+/**
+ * 递归反转
+ * @param L
+ * @param pPreNode
+ * @return
+ */
+ListNode *reverseListRecursivelyCore(LinkList L, ListNode *pPreNode) {
+    if (L == NULL) { return pPreNode; } // 将链表尾元素沿递归传递
+    ListNode *pHeadNode = reverseListRecursivelyCore(L->next, L);
+    L->next = pPreNode;
+    return pHeadNode;
+}
+
+/**
+ * 使用递归方法反转链表
+ * @param pL
+ */
+void reverseListRecursively(LinkList *pL) {
+    if (pL == NULL || (*pL)->next == NULL) { return; }
+    *pL = reverseListRecursivelyCore(*pL, NULL);
+}
+
 int main() {
     int data[6] = {1, 2, 3, 4, 5, 6};
     LinkList L;
     initListByArray(&L, data, 6);
     listPrint(L);
-    reverseList(&L); // 在无环的链表中查找
+    reverseList(&L);
     listPrint(L);
-
+    reverseListRecursively(&L);
+    listPrint(L);
     return 0;
 }

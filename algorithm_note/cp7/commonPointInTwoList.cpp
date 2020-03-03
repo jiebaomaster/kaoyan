215062aa3db0a498d93dec27eeeb1e23b04004cf
jiebaomaster <jiebaomaster@163.com>
2020-03-04 00:12:46 +0800

    [add]算法笔记-笔记-寻找链表的第一个共同节点的标记法

---
+++ algorithm_note/cp7/commonPointInTwoList.cpp
@@ -0,0 +1,45 @@
+#include <cstdio>
+
+using namespace std;
+
+/**
+ * P261 寻找链表的第一个共同节点
+ * 这里给出了链表中每一个节点的地址 A，且 A <= 10^5，则可用一个数组存储静态链表，
+ */
+const int max = 100010;
+
+struct Node { // 静态链表节点
+    char data; // 数据
+    int next; // 下一个节点地址
+    bool inList1; // 节点是否在第一个链表中
+
+    Node() {
+        inList1 = false; // 默认节点不再第一个链表中
+    }
+} node[max];
+
+int main() {
+    int l1_address, l2_address; // 两个链表的首地址
+    int n; // 总共的节点数
+    scanf("%d %d %d", &l1_address, &l2_address, &n);
+    int nodeAddress;
+    char nodeData;
+    int nodeNextAddress;
+    for (int j = 0; j < n; ++j) { // 读入所有节点建立链表
+        scanf("%d %c %d", &nodeAddress, &nodeData, &nodeNextAddress);
+        node[nodeAddress].data = nodeData;
+        node[nodeAddress].next = nodeNextAddress;
+    }
+    int p;
+    for (p = l1_address; p != -1; p = node[p].next) { // 遍历第一个链表，给其所有元素标志为在第一个链表中
+        node[p].inList1 = true;
+    }
+    for (p = l2_address; p != -1; p = node[p].next) { // 遍历第二个链表，直到遇到第一个在第一个链表中的元素，即为第一个共同节点
+        if (node[p].inList1) { break; }
+    }
+    if (p == -1) {
+        printf("-1\n");
+    } else {
+        printf("%05d", p);
+    }
+}
--- algorithm_note/summary.md
+++ algorithm_note/summary.md
@@ -58,3 +58,12 @@ double solve(double left, double right) {
     return left;
 }
 ```
+
+## cp7-数据结构专题
+
+### 链表
+
+#### 寻找两个链表的第一个公共节点
+
+1. 若节点元素可添加标记，可以使用标记法，先遍历一次标记链表一的所有节点，再遍历链表二直到遇到一个在链表一中出现的节点 [示例](./cp7/commonPointInTwoList.cpp)
+2. 若节点元素不可添加标记，可以使用双指针法，先遍历一次两个链表，双出长度之差 dif，再遍历第二次，此时快指针在较长的链表中先走 dif步，再一起走，直到当两个指针指向同一个节点时停止 [示例](../jianzhi_offer/cp5/findFirstCommonNodeInTwoList.c)

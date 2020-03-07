8fecb75a3830782ea3f6d68dbfeff89b1c2e2c73
jiebaomaster <jiebaomaster@163.com>
2020-03-07 17:13:39 +0800

    [add]经典算法-排序算法补充，堆排序

---
+++ classical/sort/heapSort.cpp
@@ -0,0 +1,69 @@
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * 向下调整
+ * @param data
+ * @param start
+ * @param end
+ */
+void adjustDown(int data[], int start, int end) {
+    while (start * 2 <= end) { // 到叶子节点时退出
+        int child = start * 2;
+        if (child + 1 <= end && data[child] < data[child + 1]) { child++; } // 选择值最大的子节点
+        if (data[start] < data[child]) { // 子节点大于父节点，需要进行交换
+            swap(data[start], data[child]);
+            start = child;
+        } else { // 父节点大于子节点，则已经成为大根堆，直接退出
+            break;
+        }
+    }
+}
+
+/**
+ * 建立大根堆
+ * @param data
+ * @param length
+ */
+void buildMaxHeap(int *data, int length) {
+    // 从最后一个非叶子节点开始，直到根节点，依次遍历所有的非叶子节点，进行向下调整
+    // 反向遍历可以保证到每次遍历后，该子树都是一个大根堆，则最后到根节点时，整个树是一个大根堆
+    for (int i = length / 2; i >= 0; --i) {
+        adjustDown(data, i, length - 1);
+    }
+}
+
+/**
+ * 堆排序，大根堆版
+ * @param data
+ * @param length
+ */
+void heapSort(int data[], int length) {
+    buildMaxHeap(data, length);
+    for (int i = length - 1; i > 0; --i) { // 每次选出（删去）堆中最大的元素（即堆顶），调整剩下元素的为大根堆
+        swap(data[0], data[i]); // 将堆顶元素和最后一个元素交换
+        adjustDown(data, 0, i - 1); // 排除最后一个元素，调整剩下的堆
+    }
+}
+
+
+int main() {
+    int n;
+    while (scanf("%d", &n) != EOF) {
+        int *pData = new int[n];
+        for (int i = 0; i < n; ++i) {
+            scanf("%d", pData + i);
+        }
+        heapSort(pData, n);
+        for (int j = 0; j < n; ++j) {
+            if (j != 0) { printf(" "); }
+            printf("%d", *(pData + j));
+        }
+        printf("\n");
+        delete[] pData;
+    }
+
+    return 0;
+}

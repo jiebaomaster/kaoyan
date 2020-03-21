d5faca74e317260a736e6171b9e351e18947e2b3
jiebaomaster <jiebaomaster@163.com>
2020-03-21 21:02:01 +0800

    [add]HDU_OJ-2097-进制转换应用

---
+++ HDU_OJ_page11/2097.cpp
@@ -0,0 +1,39 @@
+/*
+ * 判断一个数十进制十六进制十二进制表示的各位数之和是否相等
+ * http://acm.hdu.edu.cn/showproblem.php?pid=2097
+ */
+
+#include <cstdio>
+
+/**
+ * 获取某个进制表示下的各数位和
+ * @param r 进制基
+ * @param number
+ * @return
+ */
+int getSum(int r, int number) {
+    int sum = 0;
+    while (number) { // 进制转换，除基取余法
+        sum += number % r;
+        number /= r;
+    }
+    return sum;
+}
+
+int main() {
+    int n;
+    while (scanf("%d",&n)!=EOF) {
+        if (n==0) { break;}
+        int n10=getSum(10,n);
+        int n12=getSum(12,n);
+        int n16=getSum(16,n);
+        if (n10==n12&&n10==n16) {
+            printf("%d is a Sky Number.", n);
+        } else {
+            printf("%d is not a Sky Number.", n);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
